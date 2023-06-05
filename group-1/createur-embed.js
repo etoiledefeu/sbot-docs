55a0addce22ac437662e72f0e2845fb486b6f849
Etoile de Feu <108147303+etoiledefeu@users.noreply.github.com>
2023-06-05 22:41:19 +0200

    Update createur-embed.js

--- group-1/createur-embed.js
+++ group-1/createur-embed.js
@@ -1,1 +1,8 @@
+---
+description: Tutoriels sur le créateur d'embed
+---
 
+# Créateur d'embeds
+
+Sbot dispose d'un créateur d'embed (/embed).
+Mais tu ne sais pas ce que c'est un embed, ou simplement comment la commande fonctionne ? Alors cette page est faite pour toi
